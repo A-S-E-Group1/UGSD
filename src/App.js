c61c6b042317adcf4b94895ced53ad252729f39c
Young Legend <jeffdanquah16@gmail.com>
2024-08-01 15:14:35 +0000

    access level to events implemented

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import ViewAthlete from "./pages/view-athlete";
 import AddAthlete from "./pages/add-athlete";
-import Activities from "./pages/activities";
+import Activities from "./pages/calendar";
 import Login from "./pages/login";
 import ResetPassword from "./pages/reset-password";
 import EditAthlete from "./pages/edit-athlete";
@@ -12,33 +12,32 @@ import SearchPage from "./pages/search";
 import CalendarComponent from "./components/calendar/calendar";
 import PublicCalendar from "./components/calendar/publicCalender";
 
-
 function App() {
-	return (
-	  <Routes>
-		{/* Public routes accessible to all users */}
-		<Route path="/" element={<PublicHome />}>
-		  <Route index element={<SearchPage />} />
-		  <Route path="activities" element={<Activities />} />
-		  <Route path="/" element={<PublicCalendar />} />
+  return (
+    <Routes>
+      {/* Public routes accessible to all users */}
+      <Route path="/" element={<PublicHome />}>
+        <Route index element={<SearchPage />} />
+        {/* Public calendar accessible via the /activities path */}
+        <Route path="activities" element={<PublicCalendar />} />
+      </Route>
 
-		</Route>
-  
-		{/* Admin routes protected with authentication */}
-		<Route path="/admin" element={<ProtectedRoute children={<Home />} />}>
-		  <Route index element={<ViewAthlete />} />
-		  <Route path="add" element={<AddAthlete />} />
-		  <Route path="activities" element={<Activities />} />
-		  <Route path="edit/:id" element={<EditAthlete />} />
-		  <Route path="search" element={<SearchPage />} />
-		  <Route path="/admin" element={<CalendarComponent />} />
-        
-		</Route>
-  
-		{/* Public routes */}
-		<Route exact path="/login" element={<Login />} />
-		<Route exact path="/reset-password" element={<ResetPassword />} />
-	  </Routes>
-	);
-  }
+      {/* Admin routes protected with authentication */}
+      <Route path="/admin" element={<ProtectedRoute children={<Home />} />}>
+        <Route index element={<ViewAthlete />} />
+        <Route path="add" element={<AddAthlete />} />
+        <Route path="activities" element={<Activities />} />
+        <Route path="edit/:id" element={<EditAthlete />} />
+        <Route path="search" element={<SearchPage />} />
+        {/* Admin calendar under /admin/calendar */}
+        <Route path="calendar" element={<CalendarComponent />} />
+      </Route>
+
+      {/* Public routes */}
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/reset-password" element={<ResetPassword />} />
+    </Routes>
+  );
+}
+
 export default App;
