0cf534b932341d08f17d510db1027e52ae303691
Young Legend <jeffdanquah16@gmail.com>
2024-08-30 07:13:55 +0000

    added more protected route features

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -4,16 +4,20 @@ import { useGlobalContext } from "../context/context";
 import Loading from "./loading/loading";
 
 function ProtectedRoute({ children }) {
-	const { user, isLoading } = useGlobalContext();
-	if (isLoading) {
-		return <Loading />;
-	}
-	if (user) {
-		return <>{children}</>;
-	} else {
-		return <Navigate to="/login" />;
-	}
-	// return
+  const { user, isLoading } = useGlobalContext();
+
+  // Display loading screen while Firebase is checking authentication status
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  // If the user is authenticated, render the children (protected content)
+  if (user) {
+    return <>{children}</>;
+  } else {
+    // If the user is not authenticated, redirect to the login page
+    return <Navigate to="/login" />;
+  }
 }
 
 export default ProtectedRoute;
